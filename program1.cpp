c090c26f051c84cbd43e662da546c8cbe6c26150
Dua Faisal <76398671+DuaFaisal@users.noreply.github.com>
2022-05-10 18:21:34 +0500

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num;
+
+    cout << "Hello world!"<<endl;
+
+    
+cout<<"Enter any number: ";
+    
+    cin>>num;
+   
+    cout<<"The number you entered is: "<<num;
+   
+    return 0;
+
+
+}
